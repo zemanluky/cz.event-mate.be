93f178a158b252ff2d4976f94d01a50d62a59628
zemanluky <zemanluky@gmail.com>
2024-11-06 14:40:10 +0100

    :sparkles: Add example endpoint with handling body validation

--- src/auth/controller/hello-world.controller.ts
+++ src/auth/controller/hello-world.controller.ts
@@ -1,7 +1,17 @@
 import express, {type Request, type Response} from "express";
+import {fooSchema, type TFoo} from "../schema/request/foo.schema.ts";
+import {bodyValidator} from "../helper/request.validator.ts";
 
 export const helloWorldController = express.Router();
 
 helloWorldController.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
+
+helloWorldController.post('/foo',
+    bodyValidator(fooSchema), // this way the body gets validated before being passed to the controller handler
+    (req: Request<{}, {}, TFoo>, res: Response) => {
+        console.log(req.body); // type hinting when using generics in the handler params
+        res.send(req.body);
+    }
+);
