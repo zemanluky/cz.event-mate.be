cc7d259127c402ab34fca75c4919b562f000f92d
Jan <159honza159@gmail.com>
2024-12-02 21:41:53 +0100

    /filter

--- src/event/schema/db/event.schema.ts
+++ src/event/schema/db/event.schema.ts
@@ -6,8 +6,9 @@ export interface IEvent {
     name: string;
     description: string;
     date: Date;
-    location: String;
+    location: [mongoose.Decimal128, mongoose.Decimal128]; // [latitude, longitude]
     private: boolean;
+    category: string;
     ownerId: Types.ObjectId;
 }
 
@@ -17,8 +18,9 @@ const eventSchema = new Schema<IEvent, TEventModel>({
     name: { type: String, required: true },
     description: { type: String, required: false },
     date: { type: Date, required: true },
-    private: { type: Boolean, required: true },
-    location: { type: String, required: true },
+    private: { type: Boolean, required: true }, 
+    location: { type: [Number, Number], required: true }, // [latitude, longitude]
+    category: { type: String, required: true },
     ownerId: { type: Schema.Types.ObjectId, required: true },
 });
 
